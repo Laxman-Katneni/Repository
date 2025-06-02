2de21b0b9a433284dc51d9d97708c723df62e3bc
Lucky-Katneni <143445097+Lucky-Katneni@users.noreply.github.com>
2025-06-02 10:26:53 -0500

    Add files via upload

---
+++ SW1/CryptoUtilities/src/CryptoUtilitiesTest.java
@@ -0,0 +1,132 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
+
+/**
+ * @author Put your name here
+ *
+ */
+public class CryptoUtilitiesTest {
+
+    /*
+     * Tests of reduceToGCD
+     */
+
+    @Test
+    public void testReduceToGCD_0_0() {
+        NaturalNumber n = new NaturalNumber2(0);
+        NaturalNumber nExpected = new NaturalNumber2(0);
+        NaturalNumber m = new NaturalNumber2(0);
+        NaturalNumber mExpected = new NaturalNumber2(0);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(nExpected, n);
+        assertEquals(mExpected, m);
+    }
+
+    @Test
+    public void testReduceToGCD_30_21() {
+        NaturalNumber n = new NaturalNumber2(30);
+        NaturalNumber nExpected = new NaturalNumber2(3);
+        NaturalNumber m = new NaturalNumber2(21);
+        NaturalNumber mExpected = new NaturalNumber2(0);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(nExpected, n);
+        assertEquals(mExpected, m);
+    }
+
+    // 3-digit prime number
+    @Test
+    public void testReduceToGCD_133_173() {
+        NaturalNumber n = new NaturalNumber2(133);
+        NaturalNumber nExpected = new NaturalNumber2(0);
+        NaturalNumber m = new NaturalNumber2(173);
+        NaturalNumber mExpected = new NaturalNumber2(0);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(nExpected, n);
+        assertEquals(mExpected, m);
+    }
+
+    /*
+     * Tests of isEven
+     */
+
+    @Test
+    public void testIsEven_0() {
+        NaturalNumber n = new NaturalNumber2(0);
+        NaturalNumber nExpected = new NaturalNumber2(0);
+        boolean result = CryptoUtilities.isEven(n);
+        assertEquals(nExpected, n);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsEven_1() {
+        NaturalNumber n = new NaturalNumber2(1);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        boolean result = CryptoUtilities.isEven(n);
+        assertEquals(nExpected, n);
+        assertEquals(false, result);
+    }
+
+    // Large odd number
+
+    @Test
+    public void testIsEven_123453() {
+        NaturalNumber n = new NaturalNumber2(123453);
+        NaturalNumber nExpected = new NaturalNumber2(123453);
+        boolean result = CryptoUtilities.isEven(n);
+        assertEquals(nExpected, n);
+        assertEquals(false, result);
+    }
+
+    /*
+     * Tests of powerMod
+     */
+
+    @Test
+    public void testPowerMod_0_0_2() {
+        NaturalNumber n = new NaturalNumber2(0);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        NaturalNumber p = new NaturalNumber2(0);
+        NaturalNumber pExpected = new NaturalNumber2(0);
+        NaturalNumber m = new NaturalNumber2(2);
+        NaturalNumber mExpected = new NaturalNumber2(2);
+        CryptoUtilities.powerMod(n, p, m);
+        assertEquals(nExpected, n);
+        assertEquals(pExpected, p);
+        assertEquals(mExpected, m);
+    }
+
+    @Test
+    public void testPowerMod_17_18_19() {
+        NaturalNumber n = new NaturalNumber2(17);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        NaturalNumber p = new NaturalNumber2(18);
+        NaturalNumber pExpected = new NaturalNumber2(18);
+        NaturalNumber m = new NaturalNumber2(19);
+        NaturalNumber mExpected = new NaturalNumber2(19);
+        CryptoUtilities.powerMod(n, p, m);
+        assertEquals(nExpected, n);
+        assertEquals(pExpected, p);
+        assertEquals(mExpected, m);
+    }
+    // 3-digit number test
+
+    @Test
+    public void testPowerMod_174_183_192() {
+        NaturalNumber n = new NaturalNumber2(174);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        NaturalNumber p = new NaturalNumber2(183);
+        NaturalNumber pExpected = new NaturalNumber2(183);
+        NaturalNumber m = new NaturalNumber2(192);
+        NaturalNumber mExpected = new NaturalNumber2(192);
+        CryptoUtilities.powerMod(n, p, m);
+        assertEquals(nExpected, n);
+        assertEquals(pExpected, p);
+        assertEquals(mExpected, m);
+    }
+
+}
