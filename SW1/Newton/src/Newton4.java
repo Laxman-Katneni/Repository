9ff7bed688cca201682b501f54dc6b88a05f251b
Lucky-Katneni <143445097+Lucky-Katneni@users.noreply.github.com>
2025-06-02 10:31:32 -0500

    Update Newton4.java

--- SW1/Newton/src/Newton4.java
+++ SW1/Newton/src/Newton4.java
@@ -6,7 +6,7 @@ import components.simplewriter.SimpleWriter1L;
 /**
  * Put a short phrase describing the program here.
  *
- * @author Lucky Katneni
+ * @author Laxman Katneni
  *
  */
 public final class Newton4 {
